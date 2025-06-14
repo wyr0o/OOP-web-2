988b1d0a2344c7da0f96888e40c7fdcc70c899dd
wyr0o <brykin-2006@mail.ru>
2025-06-14 22:52:37 +0300

    Add files via upload

---
+++ oop web 2 zadanie 2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+class Counter {
+private:
+    int value;
+
+public:
+    Counter() : value(1) {}
+    Counter(int initial_value) : value(initial_value) {}
+
+    void increment() {
+        value++;
+    }
+
+    void decrement() {
+        value--;
+    }
+
+    int getValue() const {
+        return value;
+    }
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string answer;
+    int initial_value = 1;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> answer;
+
+    if (answer == "да") {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> initial_value;
+    }
+
+    Counter counter(initial_value);
+
+    char command;
+    while (true) {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command;
+
+        if (command == '+') {
+            counter.increment();
+        }
+        else if (command == '-') {
+            counter.decrement();
+        }
+        else if (command == '=') {
+            std::cout << counter.getValue() << std::endl;
+        }
+        else if (command == 'x') {
+            std::cout << "До свидания!" << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
